797763777e4262e7a3f4f3caced065b5eb3afbb4
sdchjjj <sdchjjj@gmail.com>
2025-11-12 13:54:24 +0900

    리소스 컴포넌트 추가

--- Source/Class_Unreal_Cpp/Private/Player/ActionCharacter.cpp
+++ Source/Class_Unreal_Cpp/Private/Player/ActionCharacter.cpp
@@ -6,6 +6,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Player/ResourceComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
@@ -28,21 +29,25 @@ AActionCharacter::AActionCharacter()
 
 	GetCharacterMovement()->bOrientRotationToMovement = true; // 이동 방향을 바라보게 회전
 	GetCharacterMovement()->RotationRate = FRotator(0, 360, 0);
+
+	Resource = CreateDefaultSubobject<UResourceComponent>(TEXT("PlayerReource"));
 }
 
 // Called when the game starts or when spawned
 void AActionCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	AnimInstance = GetMesh()->GetAnimInstance();
-	ManageStamina();
-	Stamina = MaxStamina;
+	if (GetMesh()) AnimInstance = GetMesh()->GetAnimInstance();
+	if (Resource) Resource->OnStaminaEmpty.BindDynamic(this, &AActionCharacter::SetWalkMode);
 }
 
 // Called every frame
 void AActionCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (IsSprinting) Resource->AddStamina(-DeltaTime);
+	if (IsStaminaRecovering) Resource->AddStamina(DeltaTime * RestoreMultiplier);
 }
 
 // Called to bind functionality to input
@@ -86,11 +91,11 @@ void AActionCharacter::OnRollInput(const FInputActionValue& InValue)
 {
 	if (AnimInstance.IsValid())
 	{
-		if (!AnimInstance->IsAnyMontagePlaying() && Stamina >= RollStamina)
+		if (!AnimInstance->IsAnyMontagePlaying() && Resource->HasEnoughStamina(RollStamina))
 		{
 			IsStaminaRecovering = false;
 			//SetActorRotation(GetLastMovementInputVector().Rotation());
-			Stamina -= RollStamina;
+			Resource->AddStamina(-RollStamina);
 			PlayAnimMontage(RollMontage);
 			DelayStaminaRestore();
 		}
@@ -101,7 +106,7 @@ void AActionCharacter::SetSprintMode()
 {
 	//UE_LOG(LogTemp, Log, TEXT("달리기 모드"));
 
-	if (Stamina > 0 && !GetVelocity().IsNearlyZero() && !AnimInstance->IsAnyMontagePlaying())
+	if (!GetVelocity().IsNearlyZero() && !AnimInstance->IsAnyMontagePlaying())
 	{
 		IsSprinting = true;
 		IsStaminaRecovering = false;
@@ -121,56 +126,31 @@ void AActionCharacter::SetWalkMode()
 	}
 }
 
-void AActionCharacter::DelayStaminaRestore()
-{
-	auto& TimerManager = GetWorld()->GetTimerManager();
-	TimerManager.ClearTimer(timerHandle);
-	TimerManager.SetTimer(
-		timerHandle,
-		FTimerDelegate::CreateLambda([this]()
-			{
-				if (!IsSprinting) IsStaminaRecovering = true;
-			}
-		),
-		2.0f,
-		false
-	);
-}
+//void AActionCharacter::DelayStaminaRestore()
+//{
+//	auto& TimerManager = GetWorld()->GetTimerManager();
+//	TimerManager.ClearTimer(timerHandle);
+//	TimerManager.SetTimer(
+//		timerHandle,
+//		FTimerDelegate::CreateLambda([this]()
+//			{
+//				if (!IsSprinting) IsStaminaRecovering = true;
+//			}
+//		),
+//		2.0f,
+//		false
+//	);
+//}
 
-void AActionCharacter::ManageStamina()
-{
-	GetWorld()->GetTimerManager().SetTimer(
-		StaminaManagerTimerHandle,
-		FTimerDelegate::CreateLambda([this]()
-			{
-				if (IsSprinting)
-				{
-					if (Stamina < 0.0f)
-					{
-						Stamina = 0.0f;
-						SetWalkMode();
-					}
-					else
-					{
-						Stamina -= StaminaSpendValue;
-					}
-				}
-				if (IsStaminaRecovering)
-				{
-					if (Stamina > MaxStamina)
-					{
-						Stamina = MaxStamina;
-					}
-					else
-					{
-						Stamina += (StaminaSpendValue * RestoreMultiplier);
-					}
-				}
-				UE_LOG(LogTemp, Log, TEXT("Stamina : %f"), Stamina);
-				OnStaminaChanged.Broadcast(Stamina, MaxStamina);
-			}
-		),
-		StaminaManagerSpeed,
-		true
-	);
-}
+//void AActionCharacter::ManageStamina()
+//{
+//	GetWorld()->GetTimerManager().SetTimer(
+//		StaminaManagerTimerHandle,
+//		FTimerDelegate::CreateLambda([this]()
+//			{
+//			}
+//		),
+//		StaminaManagerSpeed,
+//		true
+//	);
+//}
---
+++ Source/Class_Unreal_Cpp/Private/Player/ResourceComponent.cpp
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/ResourceComponent.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UResourceComponent::UResourceComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UResourceComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//ManageStamina();
+	Stamina = MaxStamina;
+	// ...
+	
+}
+
+
+// Called every frame
+void UResourceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UResourceComponent::AddStamina(float InValue)
+{
+	// 스태미너 변경 처리
+	Stamina += InValue;
+
+	// 스태미너를 소비하고 일정 시간 뒤에 자동재생되게 타이머 세팅
+	StaminaAutoRegenCoolTimerSet();
+
+	if (Stamina <= 0)
+	{
+		Stamina = 0.0f;
+		// 델리게이트로 스태미너가 떨어졌음을 알림
+
+		OnStaminaEmpty.Broadcast();
+	}
+	UE_LOG(LogTemp, Log, TEXT("CurrentStamina : (%.1f)"), Stamina);
+}
+
+void UResourceComponent::StaminaAutoRegenCoolTimerSet()
+{
+	GetWorld()->GetTimerManager().SetTimer(
+		StaminaAutoRegenCoolTimer,
+		[this]() {
+
+			GetWorld()->GetTimerManager().SetTimer(
+				StaminaRegenTickTimer,
+				this,
+				&UResourceComponent::StaminaRegenPerTick,
+				StaminaTickInterval,
+				true,
+				StaminaTickInterval
+			);
+
+		},
+		StaminaRegenCooltime,
+		false
+	);
+}
+
+void UResourceComponent::StaminaRegenPerTick()
+{
+	Stamina += StaminaRegenAmount;
+	if (Stamina > MaxStamina)
+	{
+		Stamina = MaxStamina;
+		GetWorld()->GetTimerManager().ClearTimer(StaminaRegenTickTimer);
+	}
+	UE_LOG(LogTemp, Log, TEXT("CurrentStamina : (%.1f)"), Stamina);
+}
+
