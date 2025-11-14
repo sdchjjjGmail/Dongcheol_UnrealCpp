d5a77ec28eeacc9294197cbcedea45f594c51e99
sdchjjj <sdchjjj@gmail.com>
2025-11-14 14:33:35 +0900

    소스 정리

--- Source/KI_UnrealCPP/Private/AnimNotify/ANS_WeaponCollisionTrigger.cpp
+++ Source/KI_UnrealCPP/Private/AnimNotify/ANS_WeaponCollisionTrigger.cpp
@@ -7,7 +7,6 @@
 void UANS_WeaponCollisionTrigger::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
 {
 	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
-	OwnerCharacter = Cast<AActionCharacter>(MeshComp->GetOwner());
 	if (OwnerCharacter.IsValid())
 	{
 		// OwnerCharacter에 자기 자신을 설정
@@ -15,17 +14,18 @@ void UANS_WeaponCollisionTrigger::NotifyBegin(USkeletalMeshComponent* MeshComp, 
 
 		OwnerCharacter->SetCollisionOn();
 	}
+	else
+	{
+		OwnerCharacter = Cast<AActionCharacter>(MeshComp->GetOwner());
+	}
 }
 
 void UANS_WeaponCollisionTrigger::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
 {
 	if (OwnerCharacter.IsValid())
 	{
-		// OwnerCharacter 설정 해제
 		UE_LOG(LogTemp, Log, TEXT("Notify Hit Off"));
-
 		OwnerCharacter->SetCollisionOff();
-		OwnerCharacter = nullptr;
 	}
 	Super::NotifyEnd(MeshComp, Animation, EventReference);
 }
